ff82a8373af05aa8bae0038b1bdf724376ce6636
Falko Richter <falko@sensorberg.com>
2014-09-26 16:36:21 +0200

    new bootstrapper

---
+++ bootstrapper/src/main/java/com/sensorberg/sdk/bootstrapper/ActionActivity.java
@@ -0,0 +1,75 @@
+package com.sensorberg.sdk.bootstrapper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.sensorberg.sdk.action.Action;
+import com.sensorberg.sdk.action.UriMessageAction;
+import com.sensorberg.sdk.resolver.BeaconEvent;
+
+public class ActionActivity extends Activity{
+
+    private static final String TAG = "ActionActivity";
+    private static final String EXTRA_BEACON_EVENT = "com.sensorberg.android.sdk.actionActivity.extra.beaconEvent";
+
+    private TextView titleTextView;
+    private TextView contentTextView;
+    private Button openURLButton;
+    private UriMessageAction messageAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_action);
+
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        contentTextView = (TextView) findViewById(R.id.contentTextView);
+        openURLButton = (Button) findViewById(R.id.openURLButton);
+
+        openURLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Intent(Intent.ACTION_VIEW, Uri.parse((messageAction).getUri()));
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        if (intent != null){
+            BeaconEvent beaconEvent = intent.getParcelableExtra(EXTRA_BEACON_EVENT);
+            Action action = beaconEvent.getAction();
+            messageAction = (UriMessageAction) action;
+
+            contentTextView.setText(messageAction.getContent());
+            titleTextView.setText(messageAction.getTitle());
+
+            Log.d(TAG, messageAction.getTitle() + messageAction.getContent() + messageAction.getUri());
+        }
+    }
+
+    public static Intent intentFor(Context context, BeaconEvent beaconEvent) {
+        Intent intent = new Intent(context, ActionActivity.class);
+        intent.putExtra(EXTRA_BEACON_EVENT, beaconEvent);
+        return intent;
+    }
+
+    /**
+     * Created by falkorichter on 26/09/14.
+     */
+    public static class activities {
+    }
+}
---
+++ bootstrapper/src/main/java/com/sensorberg/sdk/bootstrapper/SensorbergApplicationBootstrapper.java
@@ -0,0 +1,160 @@
+package com.sensorberg.sdk.bootstrapper;
+
+import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.sensorberg.sdk.Logger;
+import com.sensorberg.sdk.SensorbergService;
+import com.sensorberg.sdk.internal.Plattform;
+import com.sensorberg.sdk.presenter.PresenterConfiguration;
+import com.sensorberg.sdk.resolver.BeaconEvent;
+
+public class SensorbergApplicationBootstrapper implements Plattform.ForegroundStateListener {
+
+    private static final String TAG = "ServiceBootStrapper";
+    private final Application application;
+
+    private Messenger serviceMessenger;
+    private boolean presentationDelegationEnabled;
+
+    public void setPresentationDelegationEnabled(boolean value) {
+        presentationDelegationEnabled = value;
+        if (value) {
+            registerForPresentationDelegation();
+        }
+        else{
+            unRegisterFromPresentationDelegation();
+        }
+    }
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SensorbergService.MSG_PRESENT_ACTION:
+                    Bundle bundle = msg.getData();
+                    bundle.setClassLoader(BeaconEvent.class.getClassLoader());
+                    BeaconEvent beaconEvent = bundle.getParcelable(SensorbergService.MSG_PRESENT_ACTION_BEACONEVENT);
+                    presentBeaconEvent(beaconEvent);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    public void presentBeaconEvent(BeaconEvent beaconEvent) {
+        Intent intent = ActionActivity.intentFor(application, beaconEvent);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        application.startActivity(intent);
+    }
+
+    final Messenger messenger = new Messenger(new IncomingHandler());
+
+
+    public SensorbergApplicationBootstrapper(Application application, PresenterConfiguration presenterConfiguration, String apiKey) {
+        this(application, apiKey, false, presenterConfiguration);
+    }
+
+    public SensorbergApplicationBootstrapper(Application application, String apiKey, boolean enablePresentationDelegation, PresenterConfiguration presenterConfiguration) {
+        this.application = application;
+        this.presentationDelegationEnabled = enablePresentationDelegation;
+        Intent service = new Intent(application, SensorbergService.class);
+        service.putExtra(SensorbergService.EXTRA_PRESENTER_CONFIGURATION, presenterConfiguration);
+        service.putExtra(SensorbergService.EXTRA_API_KEY, apiKey);
+
+        application.bindService(service, beaconServiceConnection, Context.BIND_AUTO_CREATE);
+
+        application.startService(service);
+    }
+
+    @Override
+    public void hostApplicationInBackground() {
+        Logger.log.applicationStateChanged("hostApplicationInBackground");
+        if (serviceMessenger != null){
+            sendEmptyMessage(SensorbergService.MSG_APPLICATION_IN_BACKGROUND);
+            unRegisterFromPresentationDelegation();
+        }
+    }
+
+    @Override
+    public void hostApplicationInForeground() {
+        Logger.log.applicationStateChanged("hostApplicationInForeground");
+        if (serviceMessenger != null){
+            sendEmptyMessage(SensorbergService.MSG_APPLICATION_IN_FOREGROUND);
+            if (presentationDelegationEnabled){
+                registerForPresentationDelegation();
+            }
+        }
+    }
+
+    private void sendEmptyMessage(int messageType) {
+        Message message = Message.obtain(null, messageType);
+        try {
+            serviceMessenger.send(message);
+        } catch (RemoteException e) {
+            Log.e(TAG, "could not send the hostApplicationInForeground message", e);
+        }
+    }
+
+    private void unRegisterFromPresentationDelegation() {
+        sendReplyToMessage(SensorbergService.MSG_UNREGISTER_PRESENSTATION_DELEGATE);
+    }
+
+    private void sendReplyToMessage(int messageType) {
+        Message msg = Message.obtain(null, messageType);
+        msg.replyTo = messenger;
+        try {
+            serviceMessenger.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void registerForPresentationDelegation() {
+        sendReplyToMessage(SensorbergService.MSG_REGISTER_PRESENTATION_DELEGATE);
+    }
+
+    public void changeAPIToken(String newApiToken) {
+        if (serviceMessenger != null){
+            Message message = Message.obtain(null, SensorbergService.MSG_SET_API_TOKEN);
+
+            Bundle bundle = new Bundle();
+            bundle.putString(SensorbergService.MSG_SET_API_TOKEN_TOKEN, newApiToken);
+            message.obj = bundle;
+            try {
+                serviceMessenger.send(message);
+            } catch (RemoteException e) {
+                Log.e(TAG, "could not send the hostApplicationInForeground message", e);
+            }
+        }
+    }
+
+
+    private ServiceConnection beaconServiceConnection = new ServiceConnection() {
+        // Called when the connection with the service is established
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d(TAG,  "we have a connection to the service now");
+            serviceMessenger = new Messenger(service);
+            if (presentationDelegationEnabled){
+                registerForPresentationDelegation();
+            }
+        }
+
+        // Called when the connection with the service disconnects
+        public void onServiceDisconnected(ComponentName className) {
+            Log.d(TAG, "onServiceDisconnected");
+            serviceMessenger = null;
+        }
+    };
+}
