ee8d304ee8fdfc057bf9b38a5a41d419e8614216
Falko Richter <falko@sensorberg.com>
2014-09-26 18:28:52 +0200

    added the BackGroundDetector

---
+++ bootstrapper/src/main/java/com/sensorberg/sdk/bootstrapper/BackgroundDetector.java
@@ -0,0 +1,79 @@
+package com.sensorberg.sdk.bootstrapper;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.sensorberg.sdk.internal.Plattform;
+
+public class BackgroundDetector implements Application.ActivityLifecycleCallbacks {
+    private final Runnable FOREGROUND = new Runnable() {
+        @Override
+        public void run() {
+            if (!appForeGroundState && isInForeground) {
+                appForeGroundState = true;
+                foregroundStateListener.hostApplicationInForeground();
+            }
+        }
+    };
+    private final Runnable BACKGROUND = new Runnable() {
+        @Override
+        public void run() {
+            if (!isInForeground) {
+                appForeGroundState = false;
+                foregroundStateListener.hostApplicationInBackground();
+            }
+        }
+    };
+    private final Handler handler;
+    private boolean isInForeground = true;
+    private boolean appForeGroundState = isInForeground;
+    private Plattform.ForegroundStateListener foregroundStateListener = Plattform.ForegroundStateListener.NONE;
+
+    public BackgroundDetector(Plattform.ForegroundStateListener foregroundStateListener) {
+        this.handler = new Handler();
+        this.foregroundStateListener = foregroundStateListener;
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        handler.removeCallbacksAndMessages(null);
+        this.isInForeground = true;
+        handler.postDelayed(FOREGROUND, 500);
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+        handler.removeCallbacksAndMessages(null);
+        this.isInForeground = false;
+        handler.postDelayed(BACKGROUND, 500);
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+
+    }
+}
